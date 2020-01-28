ec325a2b56700c44019666f52a6483a95653b3a2
Claudio <calarcon1992@gmail.com>
2020-01-28 17:42:05 -0300

    Creación de Componentes: Solucion 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Solution1Component } from './component/solution1/solution1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Solution1Component
   ],
   imports: [
     BrowserModule,
