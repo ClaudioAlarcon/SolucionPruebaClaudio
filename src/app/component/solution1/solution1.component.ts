2d3381f925641b3ff37718e953f239e67ef2fc6d
Claudio <calarcon1992@gmail.com>
2020-01-30 14:32:21 -0300

    Creación de función para recibir los numeros del endpoint

--- src/app/component/solution1/solution1.component.ts
+++ src/app/component/solution1/solution1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EndpointService } from 'src/app/services/endpoint.service';
+import { Solution1 } from 'src/app/interfaces/solution1';
 
 @Component({
   selector: 'app-solution1',
@@ -7,10 +8,17 @@ import { EndpointService } from 'src/app/services/endpoint.service';
   styleUrls: ['./solution1.component.css']
 })
 export class Solution1Component {
-  public numbers = [];
+  public numbers: number[];
 
-  constructor(public endpoint: EndpointService) { }
+  constructor(public endpoint: EndpointService) {
+    this.numbers = [];
+   }
 
+  // Función para capturar el atributo "data" desde endpoint, retorna una lista con los números.
   public getnumbers() {
+    this.endpoint.getdata('http://patovega.com/prueba_frontend/array.php').subscribe((res: Solution1) => {
+    this.numbers = res.data;
+    return this.numbers;
+    });
   }
 }
