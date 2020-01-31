660d7968e26c14ea4e7f603003c87d2baa281c1f
Claudio <calarcon1992@gmail.com>
2020-01-30 22:41:12 -0300

    Creación de función que ordena los números.

--- src/app/component/solution1/solution1.component.ts
+++ src/app/component/solution1/solution1.component.ts
@@ -9,16 +9,55 @@ import { Solution1 } from 'src/app/interfaces/solution1';
 })
 export class Solution1Component {
   public numbers: number[];
+  count: number;
+  public arraydatanumbers: Array<number[]>;
+  public arraynumbers: number[];
+  public quantity: number[];
+  public arraycompare: number[];
 
   constructor(public endpoint: EndpointService) {
     this.numbers = [];
+    this.count = 0;
+    this.arraydatanumbers = [];
+    this.arraynumbers = [];
+    this.quantity = [];
    }
 
   // Función para capturar el atributo "data" desde endpoint, retorna una lista con los números.
   public getnumbers() {
+    this.arraydatanumbers = [];
+    this.arraynumbers = [];
     this.endpoint.getdata('http://patovega.com/prueba_frontend/array.php').subscribe((res: Solution1) => {
     this.numbers = res.data;
-    return this.numbers;
+    this.arraydatanumbers.push(this.numbers);
+    this.arraydatanumbers.forEach(datas => {
+      datas.forEach(element => {
+        this.arraynumbers.push(element);
+      });
     });
+    });
+    this.arraycompare = this.arraynumbers;
+    return this.arraynumbers;
+  }
+
+  // public quantitydata() {
+  //   this.arraynumbers.forEach(element => {
+  //     console.log(element);
+  //   });
+  // }
+
+  // Función para ordenar números
+  public sort(compare: number[]): number[] {
+    let int = 0;
+    for (let i = 0; i < (compare.length - 1); i++) {
+      for (let j = 0; j < (compare.length - i); j++) {
+        if (compare[j] > compare[j + 1]) {
+          int = compare[j];
+          compare[j] = compare[j + 1];
+          compare[j + 1] = int;
+        }
+      }
+    }
+    return compare;
   }
 }
