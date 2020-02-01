3189d59e1ee843aee12c8611893f34e0ea499247
Claudio <calarcon1992@gmail.com>
2020-02-01 19:31:55 -0300

    Creación de función que obtiene el texto desde el endpoint

--- src/app/component/solution2/solution2.component.ts
+++ src/app/component/solution2/solution2.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EndpointService } from 'src/app/services/endpoint.service';
+import { Solution2 } from 'src/app/interfaces/solution2';
 
 @Component({
   selector: 'app-solution2',
   templateUrl: './solution2.component.html',
   styleUrls: ['./solution2.component.css']
 })
-export class Solution2Component implements OnInit {
+export class Solution2Component {
   public letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
    'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  constructor() {
+  text: string[];
+  constructor(public endpoint: EndpointService) {
   }
 
-  ngOnInit() {
-  }
-
+  // Función para capturar el atributo "data" desde endpoint, retorna una lista con los números.
+  public gettext(): void {
+    this.endpoint.getdata('http://patovega.com/prueba_frontend/dict.php').subscribe((res: Solution2) => {
+    this.text = res.data;
+    console.log(this.text);
+  });
+}
 }
